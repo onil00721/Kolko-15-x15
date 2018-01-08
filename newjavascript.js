91e77f38c4be0a29483004481fe11b38bc537c1b
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-08 19:32:05 +0100

    ---,19,32

--- newjavascript.js
+++ newjavascript.js
@@ -2,15 +2,30 @@
 
     
 
-$( "#1" ).click(function() {     //akcja dla kratki 1
-     $("#1").html( "<p>X</p>");
+$( ".kratka" ).click(function() {     
+       $(" .kratka p").html("<p>X</p>");
+    $("p").css({"opacity": "0.9", "font-size": "100%"});
+    // $("#1 p").css("background-color", " blue");
+     $(" .calosc ").html('<div  class="kratka">  <p>ohh</p> </div>');
+    
 });
 
 
-
-
-
-
-              
-              
+$( "button" ).click(function() {  
     
+    for(var i=0; i<10; i++){
+    
+     $(" .rzad ").append(  '<div  class="kratka">  <p>k</p> </div>' );
+               //$("kratka").css({"opacity": "0.9", "margin:left":  "j" + "px" "font-size": "100%"});
+
+    console.log(i);
+    }
+
+              
+              
+    });
+
+
+
+
+ //<div  class="kratka">  <p>k</p> </div>
