2e05237c959358ed1235fc9522a5f6adc8ee573c
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-09 13:55:40 +0100

    -----13,55

--- Nowy folder/newjavascript.js
+++ Nowy folder/newjavascript.js
@@ -1,20 +1,32 @@
    
-
-function wstaw(){
-
-    
-         $("#div1").fadeTo("slow", 0.15);
-        $("#2").fadeTo("slow", 0.3);
-        
-   
+    var tab=[];
+for(var j=1; j<5; j+=1){
+           tab[j]=[j];
+     console.log(tab[j]+"Tab"); 
 
 }
+   
+ console.log(tab[1]); 
+ console.log(tab[2]); 
+ console.log(tab[3]); 
+ console.log(tab[4]); 
 
-       $( "#2" ).click(function() {          //wstawianie x do kratkek po kliknięciu 
-      
-    console.log("Acha");
-   wstaw();
-    
-});
 
+
+
+//for(var j=0; j<10; j+=2){
+          //  tab[j]=[j];
+              
+      //   console.log(j); 
+   //      console.log(tab[1]); 
+
+//}
+
+
+
+   
+         
+         
+        
+        
 
--- newjavascript.js
+++ newjavascript.js
@@ -1,30 +1,29 @@
 
     function click(){
 
-        $( ".kratka" ).click(function() { 
-        console.log("Coś klikłeś");
-
-         console.log("j= "+ j);
-        
-         
-        
-        for(var j=1; j<257; j++){
-              console.log("j2= "+ j);
-             var htmlString = $(".kratka").html();
-    if ( htmlString ==  '<div id=' + j + ' class="kratka">  <p>'+ j +"</p> </div>" ){
+        $( "#4" ).click(function() { 
+          
+     var htmlString = $("#4 p").html();
+   
+            console.log(htmlString + "  Jeden");
+            
+            
+            if ( htmlString ==   '4'  ){
         console.log("Coś trafiłeś");
+        console.log(htmlString + "Dwa");
         
-    }
+     
+           
         
         
-    }
+}
              });
     }
 
     
     function wstaw(){            //wstawianie x do kratkek po kliknięciu 
 
-        $( "#2" ).click(function() { 
+        $( "#2").click(function() { 
 
         $("#2").html("<p>X</p>");
         $("#2 p ").css({"opacity": "0.9", "font-size": "100%"});
@@ -39,11 +38,13 @@
 
 //$( "button" ).click(function() {  //dodawanie kolejnych kratkek o kolejnym id i takim samym numerze 
     
-    for(var i=1; i<257; i++){
+    for(var i=1; i<257; i++){   //generujemy kkolejne kwadraty 
     
      $(" .rzad ").append(  '<div id=' + i + ' class="kratka">  <p>'+ i +"</p> </div>" );
            
     }
+
+ 
 
       wstaw();    
       click();
