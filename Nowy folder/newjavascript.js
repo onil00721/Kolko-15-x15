3861343957b180f31c008a5b251ea6cce26f8ee4
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-09 09:40:39 +0100

    9,01,09,40

--- Nowy folder/newjavascript.js
+++ Nowy folder/newjavascript.js
@@ -1,13 +1,20 @@
    
 
+function wstaw(){
 
-$(document).ready(function(){
-    $("button").click(function(){
+    
          $("#div1").fadeTo("slow", 0.15);
-        $(".kratka").fadeTo("slow", 0.3);
+        $("#2").fadeTo("slow", 0.3);
         
-    });
+   
+
+}
+
+       $( "#2" ).click(function() {          //wstawianie x do kratkek po kliknięciu 
+      
+    console.log("Acha");
+   wstaw();
+    
 });
 
 
-        
--- newjavascript.js
+++ newjavascript.js
@@ -1,37 +1,62 @@
 
+    function click(){
+
+        $( ".kratka" ).click(function() { 
+        console.log("Coś klikłeś");
+
+         console.log("j= "+ j);
+        
+         
+        
+        for(var j=1; j<257; j++){
+              console.log("j2= "+ j);
+             var htmlString = $(".kratka").html();
+    if ( htmlString ==  '<div id=' + j + ' class="kratka">  <p>'+ j +"</p> </div>" ){
+        console.log("Coś trafiłeś");
+        
+    }
+        
+        
+    }
+             });
+    }
 
     
+    function wstaw(){            //wstawianie x do kratkek po kliknięciu 
 
-$( ".rzad" ).click(function() {     
-       $(" .kratka p").html("<p>X</p>");
-    $("p").css({"opacity": "0.9", "font-size": "100%"});
-    // $("#1 p").css("background-color", " blue");
-     $(" .calosc ").html('<div  class="kratka">  <p>ohh</p> </div>');
-    
-});
+        $( "#2" ).click(function() { 
+
+        $("#2").html("<p>X</p>");
+        $("#2 p ").css({"opacity": "0.9", "font-size": "100%"});
+        
+
+        });
+
+     }
+
+ 
 
 
-
-
-
-$( "button" ).click(function() {  
+//$( "button" ).click(function() {  //dodawanie kolejnych kratkek o kolejnym id i takim samym numerze 
     
     for(var i=1; i<257; i++){
     
-     $(" .rzad ").append(  '<div  class="kratka">  <p>'+ i +"</p> </div>" );
-              
-
-    console.log(i);
+     $(" .rzad ").append(  '<div id=' + i + ' class="kratka">  <p>'+ i +"</p> </div>" );
+           
     }
- //$(".kratka").css({"opacity": "0.9", "margin:left":  "j" + "px" "font-size": "100%"});
-              
-              
-    });
+
+      wstaw();    
+      click();
+   
+   
+    
+     
+    //});
 
 
 
 
- //<div  class="kratka">  <p>k</p> </div>
 
 
-  //<div class="kratka"><p>fa</p></div>
+
+
