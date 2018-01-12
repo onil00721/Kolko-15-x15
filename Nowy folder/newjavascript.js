2d9727ef3dda7f6fbe18ef417faf55f1e48eeed8
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-12 09:04:47 +0100

    12,01,09,04

--- Nowy folder/newjavascript.js
+++ Nowy folder/newjavascript.js
@@ -5,96 +5,106 @@ var tab=[];
  var tab2=[];
 for (var i=1; i<257; i++){
 tab[i]=i;
+//tab2[i]=0;
 
 
 
-  tab[87]=10  
-  tab[103]=10  
-  tab[119]=10  
-  tab[135]=10  
-  tab[151]=10  
-        
+  tab[87]=1000  
+  tab[103]=1000  
+  tab[119]=1000  
+  tab[135]=1000  
+  tab[151]=1000
+    
 }
 
+//console.log(tab.indexOf(1000));
 
+
+
+
+
+    var h=0;
 
 for (var k=1; k<=192 ; k++){   
 
    var l=1;
-    
+  
 
     for (var j=k; j<= k+65; j+=16){             //przeszukujem pola pionowo       
     
-    if (tab[j]==10){       
-       
+    if (tab[j]==1000){       
+        //console.log(tab2);
       
-        tab2[j]=j;
+    
+       
         
        
         
+        tab2[h]=j;
+         
+     //  console.log(tab2);
+        // console.log(h); 
+        
+            // console.log(tab2);
         
         
-        
- 
-        for (var y=0; y<258; y++){
-            if (tab2[y]>0){
-                // console.log(tab2[y]+ "   zawartość tab2 ");//Filtrowanie tablicy pod kontem zwycięskich pól 
-                 //console.log(tab2);
-                 //console.log(tab2[y]);
-                 tab2.sort();
-                console.log(tab2[y]);
+         l++;
+           h++;
+        if(l==6){
+             console.log("Jesteś zwycięzcą");
+       
+            
+    }}}}
+
+        tab2.sort();
+    //console.log(tab2);
+       
+            
+       var t=1;
+     var a,b,e,f,g;
+
+      for( t=0; t<tab2.length; t++){
+        console.log(tab2);
+        e=t+1;
+        f=t+1;
+        g=t+2;
+        a =tab2.slice(t,e);
+        b = tab2.slice(f,g);
+        a = parseInt(a);
+        b = parseInt(b);
+
+
+            if(a!=b){
+
+        //console.log(a + "   a");  
+               // console.log(tab2+ "    tab2 ");
+        }   
+       }
+
                
-                /*
-                                      tab2.sort();
-
-                                                    var t=1;
-                                    var a,b,e,f,g;
-
-
-
-
-                                    for( t=0; t<tab2.length; t++){
-                                        var a;
-                                        var b;
-                                        e=t+1;
-                                    f=t+1;
-                                    g=t+2;
-                                       a =tab2.slice(t,e);
-                                          b = tab2.slice(f,g);
-                                        a = parseInt(a);
-                                        b = parseInt(b);
-
-
-                                            if(a!=b){
-
-                                        //console.log(a + "   a");  
-                                                console.log(tab2[y]+ "    tab2 ");
-                                        }   
-                                       }
-
-                */
-                
-                
-            }           
-        }        
+             
+                                   
+             
+             
         
-        l++;
+     /*   l++;
          
         if(l==6){
              console.log("Jesteś zwycięzcą");
              //console.log(tab2 + "szczęśliwe numery to ");
             
-        }
         
+             
+            
+               */        
+             
+        
+     
+        
+       
       
-        
-    
-        
-      
-    }
+                                   //logika przeszukująca rzędy w pionie 
 
-}                                   //logika przeszukująca rzędy w pionie 
-}
 
 
 /*
