e9a1069fa1d6503acd5f6f983c2a49b23ec65322
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-14 17:52:19 +0100

    14,01,17,51
    To chyba koniec pracy na logiką wyznaczania zwycięzcy

--- Nowy folder/newjavascript.js
+++ Nowy folder/newjavascript.js
@@ -15,11 +15,13 @@ for (var i=1; i<257; i++){
 tab[i]=i;
 
     
-  tab[21]=1000  
-  tab[17]=1000  
-  tab[18]=1000  
-  tab[19]=1000  
-  tab[20]=1000
+  tab[144]=1000  
+  tab[129]=1000  
+  tab[114]=1000  
+  tab[99]=1000  
+  tab[84]=1000
+    
+ 
     
   
     
@@ -51,17 +53,26 @@ for (var k=1; k<=192 ; k++){
 
 var m=1;
 for (var o=m; o<=256; o++){           //wyszukujemy zwycięzkie pola w poziomie 
-     var q=0;
+    
+    var q=0;
      var w=0;       
  
-
-    if(o>=11+m){        
+/*if ( o <= r   ){
+         //console.log(a );    
+      }
+   
+    else   {
+        o+=3;
+        r+=16;
+     }*/   
+        
+    if(o>=12+m){        
         o+=4;
         m+=16;        
          }
      
      for ( var v=o; v<6+o; v++){  
-         
+          
          if (tab[v]==1000){       
              tab3[w]=v;       
              w++;   
@@ -69,7 +80,7 @@ for (var o=m; o<=256; o++){           //wyszukujemy zwycięzkie pola w poziomie
                
               if (q==5){                //zwraca informacje o numerach zwycięzkich pól w poziomie          
              console.log("hit 3 ");
-            console.log(tab3);
+         console.log(tab3);
          
            }           
         }         
@@ -78,17 +89,35 @@ for (var o=m; o<=256; o++){           //wyszukujemy zwycięzkie pola w poziomie
 }
 
 
+
+
+
+
+var r=12;
+
 for (var a=1; a<=188 ; a++){  
        var b=0;
         var c=0;
         
- for (var d=a; d<= a+68; d+=17){             //przeszukujem pola na ukos po 5 kratek i z przesunięciem w bok   
-  
+      if ( a <= r   ){
+        
+     }
+    else   {
+       a+=4;
+       r+=16;
+        
+          //    
+     
+    }
+    
+   
+ for (var d=a; d<= a+68; d+=17){             //przeszukujem pola na ukos w prawo po 5 kratek i z przesunięciem w bok   
+    //console.log(d);
      if (tab[d]==1000){       
          tab4[c]=d;       
          c++;   
          b++;
-         
+        
          if (b==5){                //zwraca informacje o numerach zwycięzkich pól 
        console.log("hit 4");                   
        console.log(tab4);                   
@@ -99,13 +128,56 @@ for (var a=1; a<=188 ; a++){
 
 
 
+/*
+var r=12;
+for (var a=1; a<=188 ; a++){  //zabezpieczenie rzędów przed złymi wartościami- rzędy na ukos w prawo 
+       
+        if ( a <= r   ){
+        
+     }
+    else   {
+       a+=4;
+       r+=16;
+        
+          // console.log("    ");   
+     
+    }
+    
+    
+ for (var d=a; d<= a+68; d+=17){             //przeszukujem pola na ukos po 5 kratek i z przesunięciem w bok   
+   //console.log(d );   
+     
+     
+     
+
+         
+        
+    }
+}
+
+*/
+
+
+var s=181;
 for (var f=192; f>=5 ; f--){  
        var g=0;
         var n=0;
    
- for (var p=f; p<=f+68; p+=15){             //przeszukujem pola na ukos po 5 kratek i z przesunięciem w bok   
+    
+     if ( f >= s   ){
         
-              
+      }
+   
+    else   {
+        f-=4;
+        s-=16;
+        // console.log("          ")
+     }
+    
+    
+ for (var p=f; p<=f+68; p+=15){             //przeszukujem pola na ukos w lewo  po 5 kratek i z przesunięciem w bok   
+        
+               //console.log(p ); 
      if (tab[p]==1000){       
          tab5[n]=p;       
          n++;   
@@ -119,5 +191,96 @@ for (var f=192; f>=5 ; f--){
       }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+ var b=12;
+for (  var a=0; a<256;a++){     //zabezpieczenie rzędów poziomych zwycięzkich    
+  
+    if ( a <= b   ){
+        // console.log(a );    
+      }
+   
+    else   {
+        a+=3;
+        b+=16;
+     }
+    }
+
+
+
+
+
+
+var b=12;
+for (var a=1; a<=188 ; a++){  //zabezpieczenie rzędów przed złymi wartościami- rzędy na ukos w prawo 
+       
+        if ( a <= b   ){
+        
+     }
+    else   {
+       a+=4;
+       b+=16;
+        
+          // console.log("    ");   
+     
+    }
+    
+    
+ for (var d=a; d<= a+68; d+=17){             //przeszukujem pola na ukos po 5 kratek i z przesunięciem w bok   
+   //console.log(d );   
+     
+     
+     
+
+         
+        
+    }
+}
+
+
+
+
+
+
+ var b=181;
+for (var f=192; f>=5 ; f--){  //przeszukujem pola na ukos w lewo 
+      
+    if ( f >= b   ){
+        
+      }
+   
+    else   {
+        f-=4;
+        b-=16;
+        // console.log("          ")
+     }
+    
+ for (var p=f; p<=f+68; p+=15){       
+   
+     
+     
+ //console.log(p)
+ }
+}
+
+
+
+
+*/
+
 
 
